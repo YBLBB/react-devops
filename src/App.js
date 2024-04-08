e48cfb2af28b75918106d52e20217984edef118d
Github YBLBBZ <Github 2582864333@qq.com>
2024-04-08 18:56:09 +0800

    修改请求方式

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import axios from'axios';
+import axios from 'axios';
 function App() {
-  useEffect(()=>{
-      axios.get('/app').then((res)=>{
-        console.log(res.data)
-      })
-  },[])
+  let [data, setData] = useState('初始值')
+  useEffect(() => {
+    console.log(1111);
+  }, [data])
+
+
+
+  function getData() {
+    axios.get('/app').then((res) => {
+      console.log(res.data)
+      setData(res.data)
+    })
+  }
+
   return (
     <div className="App">
-     111
+      <h2>{{ data }}</h2>
+
+
+      <button onClick={getData}></button>
     </div>
   );
 }
